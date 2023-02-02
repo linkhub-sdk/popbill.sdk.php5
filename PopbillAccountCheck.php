1bd0bed1e2cab900ed0c94132ce6762487b1f89d
seunghwan <lsh@linkhubcorp.com>
2023-02-02 17:38:43 +0900

    버전업데이트

--- PopbillAccountCheck.php
+++ PopbillAccountCheck.php
@@ -11,7 +11,7 @@
 * http://www.linkhub.co.kr
 * Author : Jeong Yohan (code@linkhubcorp.com)
 * Written : 2020-06-29
-* Updated : 2022-03-25
+* Updated : 2023-02-02
 *
 * Thanks for your interest.
 * We welcome any suggestions, feedbacks, blames or anything.
